b789f7e325c736c4453111a468b50970976e3f03
Jennifer Scroggins <jscroggins@adadapted.com>
2019-07-24 18:22:41 -0400

    removed superfluous packages from server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,4 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var logger = require('express-logger');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var inspect = require('util-inspect');
 var OAuth2 = require('OAuth').OAuth2;
 var fetch = require('node-fetch');
 var cors = require('cors')
@@ -12,25 +7,10 @@ const Bluebird = require('bluebird');
 fetch.Promise = Bluebird;
 
 var app = express();
-
 app.use(cors())
-
 
 var _twitterConsumerKey = "T5iG8R4TIWdXvkhv9IlbxPQia";
 var _twitterConsumerSecret = "MK1FmZxKuVU68cekU4YGbQFkp6w1N3X3SsyKTbY5f4N0EUxHNj";
-
-
-app.get('/get-token', function(req, res) {
-
-  var oauth2 = new OAuth2(_twitterConsumerKey, _twitterConsumerSecret, 'https://api.twitter.com/', null, 'oauth2/token', null);
-  oauth2.getOAuthAccessToken('', {
-      'grant_type': 'client_credentials'
-    }, function (e, access_token) {
-        bearerToken = access_token;
-        res.send({ access_token }); 
-  });
-
-});
 
 app.get('/search/:hashtag/:count/:type', function(req, res) {
   var hashtag = '#' + req.params.hashtag;
